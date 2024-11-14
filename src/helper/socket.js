db0911baa71835e312fab5fce47072c28f0de632
Tannic-Paprika <98445752+Tannic-Paprika@users.noreply.github.com>
2024-11-14 20:05:58 +0530

    Update socket.js

--- src/helper/socket.js
+++ src/helper/socket.js
@@ -1,10 +1,12 @@
-import {io} from "socket.io-client";
+import { io } from "socket.io-client";
+
 export const initSocket = async () => {
     const option = {
         'force new connection': true,
         reconnectionAttempts: 'Infinity',
         timeout: 20000,
-        transports: ['websocket']
-    }
+        transports: ['websocket', 'polling'],
+        withCredentials: true,
+    };
     return io(process.env.REACT_APP_SERVER_URL, option);
-}
+};
